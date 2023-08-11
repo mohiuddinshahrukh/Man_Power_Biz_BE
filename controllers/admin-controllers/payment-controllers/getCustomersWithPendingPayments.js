0f561170d28243471f52ee9efdac9c26ab5d1063
shahrukh <mohiuddinshahrukh@gmail.com>
2023-08-11 22:13:44 +0500

    get-customer-with-payment

--- controllers/admin-controllers/payment-controllers/getCustomersWithPendingPayments.js
+++ controllers/admin-controllers/payment-controllers/getCustomersWithPendingPayments.js
@@ -1,32 +1,27 @@
 const User = require("../../../models/userModel");
 const getCustomerWithPayments = async (req, res) => {
   try {
-    const allUsers = await User.find().populate({
+    const customers = await User.find({
+      userType: "customer",
+      bookings: { $exists: true, $not: { $size: 0 } },
+    }).populate({
       path: "bookings",
     });
-    if (!allUsers) {
-      res.json({ status: 400, error: true, msg: `Error in fetching users` });
-    } else {
-      const customers = allUsers.filter((user) => {
-        if (user.userType == "customer" && user.bookings.length > 0) {
-          return user;
-        }
+
+    if (!customers) {
+      return res.json({
+        status: 400,
+        error: true,
+        msg: `Error in fetching customers with bookings`,
       });
-      if (!customers) {
-        res.json({
-          status: 400,
-          error: true,
-          msg: `Error in fetching Customers array`,
-        });
-      } else {
-        res.json({
-          status: 200,
-          data: allUsers,
-          error: false,
-          msg: `Error in fetching Customers array`,
-        });
-      }
     }
+
+    res.json({
+      status: 200,
+      data: customers,
+      error: false,
+      msg: `Successfully fetched customers with bookings`,
+    });
   } catch (error) {
     res.json({
       status: 400,
