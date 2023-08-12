69096bc06e4921addee1a9be1271199f3a6c6fb1
shahrukh <mohiuddinshahrukh@gmail.com>
2023-08-12 16:37:58 +0500

    123

--- controllers/admin-controllers/booking-controllers/addBooking.js
+++ controllers/admin-controllers/booking-controllers/addBooking.js
@@ -22,15 +22,37 @@ const addBooking = asyncHandler(async (req, res) => {
       bookingPaymentStatus,
       bookingPaidAmount,
       bookingId,
+      bookingServices, // Assuming this is an array of services
     } = req.body;
 
-    // Validate customer
+    if (
+      !bookingCity ||
+      !bookingZip ||
+      !bookingDate ||
+      !bookingPackage ||
+      !bookingCustomer ||
+      !bookingContactNumber ||
+      !bookingEmailAddress ||
+      !bookingDescription ||
+      !bookingStatus ||
+      !bookingPrice ||
+      !bookingPaymentStatus ||
+      (bookingPaymentStatus.toUpperCase() === "CARD" && !bookingPaidAmount) ||
+      !bookingService
+    ) {
+      return res.status(400).json({
+        error: true,
+        msg: "Missing required fields",
+      });
+    }
+
     if (!mongoose.Types.ObjectId.isValid(bookingCustomer._id)) {
       return res.status(400).json({
         error: true,
         msg: "Invalid customer ID",
       });
     }
+
     const fetchCustomer = await User.findById(bookingCustomer._id);
     if (!fetchCustomer) {
       return res.status(400).json({
@@ -39,13 +61,13 @@ const addBooking = asyncHandler(async (req, res) => {
       });
     }
 
-    // Create booking package array
     const packageArray = bookingPackage.map((pkg) => ({
       package: pkg.package._id,
       quantity: pkg.quantity,
     }));
 
-    // Create booking
+    const serviceArray = bookingServices; // Assuming bookingService is an array of service IDs
+
     const newBooking = await Booking.create({
       bookingId:
         bookingId ||
@@ -56,6 +78,7 @@ const addBooking = asyncHandler(async (req, res) => {
       bookingZip,
       bookingDate,
       bookingPackage: packageArray,
+      bookingService: serviceArray,
       bookingCustomer: fetchCustomer._id,
       bookingContactNumber,
       bookingEmailAddress,
@@ -63,41 +86,48 @@ const addBooking = asyncHandler(async (req, res) => {
       bookingStatus,
       bookingPrice,
       bookingPaymentStatus:
-        bookingPaymentStatus.toUpperCase() == "CARD"
-          ? "FULL"
-          : bookingPaymentStatus.toUpperCase(),
+        bookingPaymentStatus.toUpperCase() === "CARD" ? "FULL" : bookingPaymentStatus.toUpperCase(),
       bookingPaidAmount:
-        bookingPaymentStatus.toUpperCase() == "CARD" ? bookingPrice : 0,
+        bookingPaymentStatus.toUpperCase() === "CARD" ? bookingPrice : 0,
       bookingRemainingAmount: Math.max(bookingPrice - bookingPaidAmount, 0),
     });
 
-    const updateUser = await User.findByIdAndUpdate(bookingCustomer, {
+    const updateUser = await User.findByIdAndUpdate(bookingCustomer._id, {
       $push: { bookings: newBooking._id },
     });
+
     if (!updateUser) {
-      res.json({
+      return res.status(400).json({
         error: true,
-        status: 400,
         msg: "User couldn't be updated.",
       });
-    } else {
-      // Update package bookings
-      await Promise.all(
-        packageArray.map(async (pkg) => {
-          const fetchedPackage = await Package.findById(pkg.package);
-          if (fetchedPackage) {
-            fetchedPackage.packageBookings.push(newBooking._id);
-            await fetchedPackage.save();
-          }
-        })
-      );
-
-      return res.status(200).json({
-        error: false,
-        data: newBooking,
-        msg: "Booking created successfully",
-      });
     }
+
+    await Promise.all(
+      packageArray.map(async (pkg) => {
+        const fetchedPackage = await Package.findById(pkg.package);
+        if (fetchedPackage) {
+          fetchedPackage.packageBookings.push(newBooking._id);
+          await fetchedPackage.save();
+        }
+      })
+    );
+
+    await Promise.all(
+      serviceArray.map(async (serviceId) => {
+        const fetchedService = await Service.findById(serviceId);
+        if (fetchedService) {
+          fetchedService.serviceBookings.push(newBooking._id);
+          await fetchedService.save();
+        }
+      })
+    );
+
+    return res.status(200).json({
+      error: false,
+      data: newBooking,
+      msg: "Booking created successfully",
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
